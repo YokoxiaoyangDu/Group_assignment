05bc45e471887b7900730d9e50bba4f0e44dac42
Yoko Xiaoyang Du <xiaoyang.du@ki.se>
2025-04-21 18:44:45 +0200

    add NAMESPACE

--- slidingwindow/R/slidingwindowfunction.R
+++ slidingwindow/R/slidingwindowfunction.R
@@ -2,6 +2,7 @@
 #'
 #' @param file_path Path to the CSV file
 #' @return a data frame
+#' @export
 read_data_frame <- function(file_path) {
   temp <- read.csv(file_path)
   temp$time <- as.POSIXct(temp$time, origin = "1970-01-01")
@@ -16,6 +17,7 @@ data_acceleration<- read_data_frame("C:/Users/xiaodu/Downloads/Example_window.cs
 #' @param y y axis
 #' @param z z axis
 #' @return a data frame
+#' @export
 combo_axis <- function(data,x,y,z) {
   temp <- data
   temp$acceleration<- sqrt(temp$x^2 + temp$y^2 + temp$z^2)
@@ -26,6 +28,7 @@ combo_axis <- function(data,x,y,z) {
 #' calculate mean of the window
 #' @param window window value
 #' @return mean of the window
+#' @export
 window_mean <- function(window) {
   return(mean(window))
 }
@@ -33,6 +36,7 @@ window_mean <- function(window) {
 #' Assign binary +1 or -1
 #' @param window window value
 #' @return +1 or -1 given calculation
+#' @export
 window_binary <- function(window) {
   diff <- window[length(window)] - window[1]
   if (diff > 0)
@@ -47,6 +51,7 @@ window_binary <- function(window) {
 #' @param window_size size of the sliding window
 #' @param window_function functions created above
 #' @return a data frame
+#' @export
 window_loop <- function(data, window_size, window_function, ...) {
   n <- nrow(data)
   result <- rep(NA, n)
@@ -66,6 +71,7 @@ window_loop <- function(data, window_size, window_function, ...) {
 #' @param use_mean show mean results
 #' @param use_binary show binary results
 #' @return a data frame
+#' @export
 plot_accelaration <- function(data,use_mean = TRUE,use_binary = TRUE) {
   figure <- ggplot2::ggplot(data, ggplot2::aes(x = time)) +
     ggplot2::geom_line(ggplot2::aes(y = acceleration), color = "blue") +
